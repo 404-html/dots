e517a232776944189313500468e39c865dde5d63
verschmelzen <komiva@bk.ru>
2019-03-11 23:57:45 +0300

    Update YCM config

--- .vim/.ycm_extra_conf.py
+++ .vim/.ycm_extra_conf.py
@@ -4,13 +4,12 @@ import ycm_core
 flags = [
   '-Wall',
   '-Wextra',
-  '-Werror',
   '-Wno-long-long',
   '-Wno-variadic-macros',
   '-fexceptions',
   '-ferror-limit=10000',
   '-DNDEBUG',
-  '-std=c99',
+  '-std=c11',
   '-xc',
   '-isystem/usr/include/',
   ]
